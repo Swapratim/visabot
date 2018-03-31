145a4f239459ffda373de846d620005bc83d1bad
Swapratim <Swapratim@users.noreply.github.com>
2018-03-31 22:19:15 +0200

    Add files via upload

--- visabot.py
+++ visabot.py
@@ -310,11 +310,15 @@ def wikipedia_search(reqContext):
                #visa_status = visa_status_temp_2[0]
                print ("2nd CATEGORY: FINAL visa_status -->" + visa_status_temp_1[1].split(")")[0])
         else:
-           visa_status = visa_status_primary[0]
+           #visa_status = visa_status_primary[0]
            print ("ELSE: No exception found, so assigning same variable to visa_status_temp_1-->" + visa_status)
            # 5th CATEGORY like, Thailand}}\n| Visa on arrival\n| e-Visa for 60 Days\n|-\n|
-           if "Visa" in visa_status_primary[0] or "*Visitor*" in visa_status_primary[0]:
+           if "Visa" in visa_status_primary[0]:
                if "}}\n|" in visa_status_primary[0]:
+                   visa_status = visa_status_primary[0].split("\n|")[1]
+                   print ("5th CATEGORY --->" + visa_status)
+            elif "Visitor" in visa_status_primary[0]:
+                   if "}}\n|" in visa_status_primary[0]:
                    visa_status = visa_status_primary[0].split("\n|")[1]
                    print ("5th CATEGORY --->" + visa_status)
     else:
