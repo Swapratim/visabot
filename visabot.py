e2c8ab593eafe629654ae8a69a2123fe645ebb1b
Swapratim <Swapratim@users.noreply.github.com>
2018-03-31 11:19:28 +0200

    Add files via upload

--- visabot.py
+++ visabot.py
@@ -280,7 +280,8 @@ def wikipedia_search(reqContext):
     # Check all artifacts which are to be removed to get the VISA INFORMATION
     print (infotoStringFinal)
     if "Visa" in infotoStringFinal:
-        visa_status_primary = infotoStringFinal.split("}} \n| {{",1)[1] 
+        print ("Size 0 -->" + infotoStringFinal.split("\n| {{",1)[0])
+        visa_status_primary = infotoStringFinal.split("\n| {{",1)[1] 
         print ("After splitting \n| {{, here is the 2nd part -->" + visa_status_primary)
     elif "}} (" in infotoStringFinal:
         visa_status_primary = infotoStringFinal.split("}} (",1)[1]
