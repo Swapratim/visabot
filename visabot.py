7b814ba887c187da115d87122fb9f2c22c717c07
Swapratim <Swapratim@users.noreply.github.com>
2018-04-11 17:24:47 +0200

    Add files via upload

--- visabot.py
+++ visabot.py
@@ -136,13 +136,13 @@ def welcome():
                 ],
             "telegram": {
                  "photo": "http://kredist.ru/wp-content/uploads/2014/10/%D0%B2%D1%8B%D0%B5%D0%B7%D0%B4-%D0%B7%D0%B0-%D0%B3%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D1%83-%D1%81-%D0%B4%D0%BE%D0%BB%D0%B3%D0%B0%D0%BC%D0%B8.jpg",
-                 "text": "Welcome in Telegram -->" + first_name + "! " + speech1 + "So let's start, shall we?",
                  "reply_markup": { 
                    "inline_keyboard": [ 
                         [{ "callback_data": "Yeah Sure", "text": "Yeah Sure" }], 
                         [{ "callback_data": "No Thanks", "text": "No Thanks" }] 
                        ] 
                 },
+                "text": "Welcome in Telegram -->" + first_name + "! " + speech1 + "So let's start, shall we?",
                }
               }
             };
