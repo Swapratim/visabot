7dbe8f6c27f369b4a908f5ad48a0ea245f984278
Swapratim <Swapratim@users.noreply.github.com>
2018-04-12 08:34:09 +0200

    Add files via upload

--- visabot.py
+++ visabot.py
@@ -149,33 +149,7 @@ def welcome():
                },
             "skype": {
               "text": "Hey!! Up for a foreign trip? Here's the easiest way to find foreign country Visa requirement.",
-              "attachments": [
-                {
-                  "contentType": "application/vnd.microsoft.card.hero",
-                  "content": {
-                    "title": "Visa CheckBot",
-                    "subtitle": "Easiest way to find Visa requirement",
-                    "images": [
-                      {
-                        "url": "http://kredist.ru/wp-content/uploads/2014/10/%D0%B2%D1%8B%D0%B5%D0%B7%D0%B4-%D0%B7%D0%B0-%D0%B3%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D1%83-%D1%81-%D0%B4%D0%BE%D0%BB%D0%B3%D0%B0%D0%BC%D0%B8.jpg"
-                      }
-                    ],
-                    "buttons": [
-                      {
-                        "type": "imBack",
-                        "title": "Yeah Sure",
-                        "value": "Yeah Sure"
-                      },
-                      {
-                        "type": "imBack",
-                        "title": "No Thanks",
-                        "value": "No Thanks"
-                      }
-                    ]
-                  }
-                }
-              ]
-            }
+             }
           }
         };
     print (res)
