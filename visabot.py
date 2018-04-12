7b3a2c62e287445397ae9193f84a26964224761c
Swapratim <Swapratim@users.noreply.github.com>
2018-04-12 16:04:10 +0200

    Add files via upload

--- visabot.py
+++ visabot.py
@@ -156,7 +156,7 @@ def welcome():
                         "content":{
                                     "title": "Welcome to SKYPE",
                                     "subtitle": "Visa CheckBot",
-                                    "images":[{"http://kredist.ru/wp-content/uploads/2014/10/%D0%B2%D1%8B%D0%B5%D0%B7%D0%B4-%D0%B7%D0%B0-%D0%B3%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D1%83-%D1%81-%D0%B4%D0%BE%D0%BB%D0%B3%D0%B0%D0%BC%D0%B8.jpg":}],
+                                    "images":"http://kredist.ru/wp-content/uploads/2014/10/%D0%B2%D1%8B%D0%B5%D0%B7%D0%B4-%D0%B7%D0%B0-%D0%B3%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D1%83-%D1%81-%D0%B4%D0%BE%D0%BB%D0%B3%D0%B0%D0%BC%D0%B8.jpg",
                                     "buttons":[{
                                         "type":"postBack",
                                         "title": "Yeah Sure",
