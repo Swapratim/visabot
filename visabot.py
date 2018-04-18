519fe6e612ba5ffeed2fcf135d5ec686980150c2
Swapratim <Swapratim@users.noreply.github.com>
2018-04-18 13:50:19 +0200

    Add files via upload

--- visabot.py
+++ visabot.py
@@ -607,11 +607,8 @@ def wikipedia_search(reqContext):
         elif destinationcountry in info and "|state<!" in info:
            print ("Exception handling for Peru, Belgium and Costa Rica-->" + info)
            #Peru|state<!--Use state flag-->}}\n| {{yes|Visa not required}}<ref>{{Timatic|nationality=CA|destination=PE|accessdate=26 November 2013}}</ref>\n| 183 days\n|\n|- \n| 
-           #infotoStringFinal = info.split("|state<!")[0]
-           print ("1111-->" + info.split("<ref>")[0])
-           print ("2222-->" + info.split("<ref>")[0].split("{{")[1])
-           print ("3333-->" + info.split("<ref>")[0].split("{{")[1].split("}}")[0].split("|")[1])
-           #infotoStringFinal = info.split("}}\n| {{")[1].split("}}<ref>")[0].split("|")[1]
+           #print ("Visa Status-->" + info.split("<ref>")[0].split("{{")[1].split("}}")[0].split("|")[1])
+           infotoStringFinal = info.split("<ref>")[0].split("{{")[1].split("}}")[0].split("|")[1]
            print ("Country name: -->" + infotoStringFinal)
         else:
            continue
